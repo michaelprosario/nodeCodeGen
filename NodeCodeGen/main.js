b1f42e383695a6b66bec701cc5efa706044519ea
Michael <michael.p.rosario@gmail.com>
2024-05-11 16:17:42 -0400

    chore: Add Bootstrap CDN for styling and scripts in edit-blazor-form.ejs

--- NodeCodeGen/main.js
+++ NodeCodeGen/main.js
@@ -1,40 +1,7 @@
 let ejs = require('ejs');
 let fs = require('fs');
 
-let renderCodeForEntityCommand = {
-    "entityName": "TimeEntry",
-    "entitySchema": {
-        "$schema": "http://json-schema.org/draft-07/schema#",
-        "type": "object",
-        "properties": {
-            "Id": {
-                "type": "string"
-            },
-            "ProjectId": {
-                "type": "string"
-            },
-            "UserId": {
-                "type": "string"
-            },
-            "Date": {
-                "type": "string",
-                "format": "date-time"
-            },
-            "StartTime": {
-                "type": "string",
-                "format": "date-time"
-            },
-            "EndTime": {
-                "type": "string",
-                "format": "date-time"
-            },
-            "Notes": {
-                "type": "string"
-            }
-        },
-        "required": ["Id", "ProjectId", "UserId", "Date", "StartTime", "EndTime", "Notes"]
-    }
-}
+let renderCodeForEntityCommand = require('./renderCodeForEntityCommand.json');
 
 // create function to extract a list of properties from the renderCodeForEntityCommand
 function extractProperties(renderCodeForEntityCommand) {
@@ -56,7 +23,7 @@ let templateInputData = {
     properties: extractProperties(renderCodeForEntityCommand)
 }
 
-console.log(templateInputData);
+// console.log(templateInputData);
 
 // load the template file 'edit-blazor-form.ejs'
 let template = fs.readFileSync('edit-blazor-form.ejs', 'utf8');
